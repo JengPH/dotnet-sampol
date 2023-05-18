01b83e1c1c4400b6ce588fb34adcc23136c4ca1a
JengPH <ejverdida22@gmail.com>
2023-05-18 21:50:00 +0800

    final modal

--- Controllers/NewAPIController.cs
+++ Controllers/NewAPIController.cs
@@ -36,14 +36,66 @@ namespace JengApp.Controllers
                     Price = p.Price,
                     Status = p.Status
                 }).ToList();
-            return prods;
+            return Ok(prods);
         }
-          public IActionResult saveCategory(Product p){
-            p.Status = "Active";
+      
+      public IActionResult addProduct(Product p)
+        {
+            if(string.IsNullOrEmpty(p.Status) || p.Status == "false")
+            {
+                p.Status = "Inactive";
+            }
             _context.Products.Add(p);
             _context.SaveChanges();
             return Ok();
         }
-        
+
+        public IActionResult updateProduct(Product p)
+        {
+            if(string.IsNullOrEmpty(p.Status) || p.Status == "false")
+            {
+                p.Status = "Inactive";
+            }
+            else
+            {
+                p.Status = "Active";
+            }
+            _context.Products.Update(p);
+            _context.SaveChanges();
+            return Ok();
+        } 
+
+         public IActionResult deleteProduct(int id)
+        {
+            var res = _context.Products.Where(q => q.Id == id).FirstOrDefault();
+            _context.Products.Remove(res);
+            _context.SaveChanges();
+            
+            return Ok();
+        }
+
+        public IActionResult addStockProduct(Product p, int iStock,string date)
+        {
+            Stockhistory sh = new Stockhistory();
+
+            p.Stock += iStock;
+            _context.Products.Update(p);
+
+            sh.AddedStock = iStock;
+            sh.ProdId = p.Id;
+            sh.Date = date;
+
+            _context.Stockhistories.Add(sh);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        public ActionResult<List<Product>> viewStockHistory(int id){
+            
+            //return _context.Products.ToList();
+            var res = _context.Stockhistories.ToList().Where(p => p.ProdId == id);
+
+            return Ok(res);
+        }     
     }
 }
